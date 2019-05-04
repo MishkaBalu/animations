3d70786ca334a1eed6cb3c416af3ab8781a35b20
Leonid Shkolnyk <leonid.shkolnyk@appus.software>
2019-05-04 18:36:06 +0300

    Project setup. Install Lottie pod

---
+++ animations/animations/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  animations
+//
+//  Created by leonid.shkolnyk on 5/4/19.
+//  Copyright © 2019 leonid.shkolnyk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
